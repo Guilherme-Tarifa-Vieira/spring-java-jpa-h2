b9512f02d94366af032f75daacaf60755e947900
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-02-12 19:09:15 -0300

    "JPA and H2  - Spring Test (DepartmentUsers)"

---
+++ target/classes/import.sql
@@ -0,0 +1,20 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informática');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Jorge', 'jorge@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'rafa', 'rafa@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'luis', 'luis@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'elen', 'elen@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'joel', 'joel@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'fausto', 'fausto@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Jonathan', 'jhouw@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'leo', 'leo@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'dani', 'dani@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'norberto', 'norb@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'hadrid', 'hadrid@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'percyJ', 'pj@gmail.com');
+
